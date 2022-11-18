4d4c06b9672d14ddfaa52917271db9c111f369f2
jayviswisely <104494669+jayviswisely@users.noreply.github.com>
2022-11-18 21:42:14 +0700

    Adjusted speed

--- snake/snake.js
+++ snake/snake.js
@@ -32,7 +32,7 @@ window.onload = function() {
     placeFood();
     document.addEventListener("keyup", changeDirection);
     // update();
-    setInterval(update, 1000/10); //100 milliseconds
+    setInterval(update, 1100/10); //100 milliseconds
 }
 
 function update() {
